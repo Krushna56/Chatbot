9c093eb5b3bae76429c5fb698297f9ea656dbcf2
Krushna <krushanakumbhar314@gmail.com>
2025-04-11 19:14:00 +0530

    modify UI

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,41 +1,27 @@
 from flask import Flask, render_template, request, jsonify
-import pickle
+import joblib
+import pandas as pd 
 import nltk
 import string
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression 
 
-# Load model and vectorizer
-model = pickle.load(open("chatbot_model.pkl", "rb"))
-vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
 
-# NLTK setup
-nltk.data.path.append("C:/Users/krush/AppData/Roaming/nltk_data")
-nltk.download('punkt')
+# Load trained chatbot model
+model = joblib.load("chatbot_train.pkl")
 
-from nltk.corpus import stopwords
-stop_words = set(stopwords.words('english'))
-
-# Flask app
 app = Flask(__name__)
 
-# Preprocessing function
-def preprocess(text):
-    text = text.lower()
-    text = ''.join([c for c in text if c not in string.punctuation])
-    tokens = nltk.word_tokenize(text)
-    return ' '.join([word for word in tokens if word not in stop_words])
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-# Route
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/get", methods=["POST"])
+def get_bot_response():
+    user_input = request.json["msg"]
+    response = model.predict([user_input])[0]
+    return jsonify({"response": response})
 
-@app.route('/get', methods=['POST'])
-def chatbot_response():
-    user_input = request.form['msg']
-    clean_input = preprocess(user_input)
-    vector_input = vectorizer.transform([clean_input])
-    response = model.predict(vector_input)[0]
-    return jsonify({'response': response})  
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
