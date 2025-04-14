c0eedbe2ab50ae8cc9060958acf927fc0bea0461
Krushna <krushanakumbhar314@gmail.com>
2025-04-14 16:53:48 +0530

    revert the commit created initialy

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,30 +1,64 @@
-from flask import Flask, render_template, request, jsonify
-import pandas as pd
-import joblib
+from flask import Flask, render_template, request
+import random
+import json
+import pickle
+import numpy as np
+from tensorflow.keras.models import load_model
+from nltk.stem import WordNetLemmatizer
+import nltk
 
-# Load trained chatbot model and vectorizer
-try:
-    model = joblib.load('model.joblib')
-    vectorizer = joblib.load('vectorizer.joblib')
-except Exception as e:
-    print(f"Error loading model or vectorizer: {e}")
-    raise
+lemmatizer = WordNetLemmatizer()
+model = load_model('chatbot_model.h5')
+intents = json.loads(open('data.json').read())
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
 
 app = Flask(__name__)
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for s in sentence_words:
+        for i, w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+def get_response(ints, intents_json):
+    if len(ints) == 0:
+        return "Sorry, I didn't understand that."
+    tag = ints[0]['intent']
+    for i in intents_json['intents']:
+        if i['tag'] == tag:
+            return random.choice(i['responses'])
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/get", methods=["POST"])
-def get_bot_response():
-    user_input = request.json["msg"]
-    # Vectorize the input text
-    input_vectorized = vectorizer.transform([user_input])
-    response = model.predict(input_vectorized)[0]
-    return jsonify({"response": response})
+@app.route("/get", methods=["GET", "POST"])
+def chatbot_response():
+    msg = request.args.get("msg")
+    ints = predict_class(msg)
+    res = get_response(ints, intents)
+    return res
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
